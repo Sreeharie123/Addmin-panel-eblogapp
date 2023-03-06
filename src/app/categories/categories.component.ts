215e25ce722a9e8c1ac3218913d85ec09c8665a1
Sreeharie123 <sreehariengoli@gmail.com>
2023-03-06 21:29:28 +0530

    Admin pannel started

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -4,22 +4,25 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor(private firestore:AngularFirestore){}
+  onSubmit(formdata: any) {
+    const categorydata = {
+      category: formdata.value.category,
+    };
 
-  onSubmit(formdata:any){
-    const categorydata={
-        category:formdata.value.category
-    }
-
-    this.firestore.collection('categories').add(categorydata).then(docRef=>{
-      console.log(docRef)
-    }).catch(err=>{
-      console.log(err)
-    })
+    this.firestore
+      .collection('categories')
+      .add(categorydata)
+      .then((docRef) => {
+        console.log(docRef);
+        formdata.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
 }
