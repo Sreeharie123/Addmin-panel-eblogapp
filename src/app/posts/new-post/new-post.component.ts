8b90db854475b221961d71e35a5e989061962b76
Sreeharie123 <sreehariengoli@gmail.com>
2023-03-08 21:22:56 +0530

    Angular material added

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
@@ -10,12 +11,24 @@ export class NewPostComponent {
   clear2=""
 
   permalink?:string;
+  imgsrc:any='https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'
 
   onTitleChanged($event:any){
 
  const title=$event.target.value
  let permlink=title.replaceAll(' ','-')
    this.permalink=permlink
+
+  }
+
+  showPreview($event:any){
+
+    const reader= new FileReader()
+    reader.onload=(e)=>{
+    this.imgsrc=e.target?.result
+    console.log(e)
+    }
+     reader.readAsDataURL($event.target.files[0])
 
   }
 
